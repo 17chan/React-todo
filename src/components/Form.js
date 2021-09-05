9275c38b088db25cd4e1a1725f6bbe2b8c418e1b
17chan <yumenonakanohiyoko@gmail.com>
2021-09-05 15:08:43 +0900

    add and delete tasks

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,18 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 
-function handleSubmit(e) {
-    e.preventDefault();
-    alert('Hello, world!');
-}
 
 
 function Form(props) {
+    function handleChange(e) {
+        setName(e.target.value);
+    }
+    const [name, setName] = useState('');
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.addTask(name);
+        setName("");
+    }
     return (
         <form onSubmit={handleSubmit}>
             <h2 className="label-wrapper">
@@ -20,6 +26,8 @@ function Form(props) {
                 className="input input__lg"
                 name="text"
                 autoComplete="off"
+                value={name}
+                onChange={handleChange}
             />
             <button type="submit" className="btn btn__primary btn__lg">
                 Add

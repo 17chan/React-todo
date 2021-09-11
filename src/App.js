a16f33a25a88e13185e604b0990027c4ec747e9f
17chan <yumenonakanohiyoko@gmail.com>
2021-09-11 14:32:26 +0900

    set the focus indicator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 // import React from "react";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { nanoid } from "nanoid";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 function App(props) {　// propsにはindex.jsからもらった tasks={DATA} のみが入ってる
   const [tasks, setTasks] = useState(props.tasks);　//state を作ってる
@@ -91,10 +99,17 @@ function App(props) {　// propsにはindex.jsからもらった tasks={DATA} �
   ));
 
 
-  // const headingText = `${taskList.length} tasks remaining`;
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
-
+  const listHeadingRef = useRef(null);
+  
+  const prevTaskLength = usePrevious(tasks.length);
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+  
 
   return (
     <div className="todoapp stack-large">
@@ -102,7 +117,9 @@ function App(props) {　// propsにはindex.jsからもらった tasks={DATA} �
       <div className="filters btn-group stack-exception">
         {filterList}    {/* All Active Completedのボタンを表示している */}
       </div>
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+        {headingText}
+      </h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
